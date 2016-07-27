4e1e1c8e911097bd1bd084c9c9dc8a180483cd0d
rkettelerij <richard@mindloops.nl>
2016-07-27 11:27:55 +0200

    Truncate story titles in story stacker + added display method for story details

---
+++ Assets/Scripts/StoryDetailsOutput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class StoryDetailsOutput : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameObject.SetActive (false);
+	}
+
+	void Display (StoriesModel.StoryModel story)
+	{
+		var textFields = this.gameObject.GetComponentsInChildren<Text> ();
+		foreach (var textField in textFields) {
+			if (textField.name.Equals ("StoryTitle")) {
+				textField.text = story.Title;
+			} else if (textField.name.Equals ("StoryDescription")) {
+				textField.text = story.Description;
+			} else if (textField.name.Equals ("StoryPoints")) {
+				textField.text = story.Points.ToString ();
+			} else {
+				throw new InvalidOperationException ("Unknown textfield: " + textField.name);
+			}
+		}
+		gameObject.SetActive (true);
+	}
+}
--- Assets/Scripts/StoryStacker.cs
+++ Assets/Scripts/StoryStacker.cs
@@ -10,7 +10,7 @@ public class StoryStacker : MonoBehaviour {
 
 		for (var i=0; i<storiesModel.Stories.Count; i++) {
 			GameObject storyBlock = (GameObject) Instantiate(storyBlockObject, new Vector3(0, 0.50f + (i * 0.16f), 2.0f), Quaternion.identity);
-			storyBlock.GetComponent<StoryController> ().SetTextValue (storiesModel.Stories[i].Title);
+			storyBlock.GetComponent<StoryController> ().SetTextValue (storiesModel.Stories[i].TitleSummary);
 		}
 	}
 }
