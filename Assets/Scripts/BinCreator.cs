366329e3905f8fd6ba3a9baa1750dbcd202c6eba
timprijn <tim@mindloops.nl>
2016-07-27 14:51:43 +0200

    Creation of the bin for deleting tickets.

---
+++ Assets/Scripts/BinCreator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+using UnityEngine.VR;
+
+public class BinCreator : MonoBehaviour {
+
+	public GameObject bin;
+
+	// Use this for initialization
+	void Start () {
+		CVRChaperone chaperone = VRSettings.enabled ? OpenVR.Chaperone : null;
+		var rect = new HmdQuad_t();
+		bool success = (chaperone != null) && chaperone.GetPlayAreaRect(ref rect);
+		var playAreaEdge = 1.5f;
+		if (success)
+		{
+			playAreaEdge = rect.vCorners2.v2 - 0.7f;
+		}
+
+		Instantiate(bin, new Vector3(-0.6f, 0.0f, playAreaEdge), Quaternion.identity);
+	}
+
+}
