476f6c35bacbd3b23ca74239e21d6b1d9c761057
Peter Hendriks <peerkehendriks@gmail.com>
2016-07-27 10:13:30 +0200

    Add .gitattributes to avoid merging unity files automatically by git.

---
+++ Assets/Scripts/PickupTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupTrigger : MonoBehaviour {
+
+	void OnTriggerStay(Collider collider)
+    {
+        Debug.Log("Collided with " + collider.name);
+    }
+}
