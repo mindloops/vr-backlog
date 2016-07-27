f3f130c5d33d168b5bc21e388f04af2c30b64746
Peter Hendriks <peerkehendriks@gmail.com>
2016-07-27 11:56:11 +0200

    Added Trigger drag drop

--- Assets/Scripts/PickupTrigger.cs
+++ Assets/Scripts/PickupTrigger.cs
@@ -1,19 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PickupTrigger : MonoBehaviour {
+public class PickupTrigger : MonoBehaviour
+{
 
-	void OnTriggerEnter(Collider collider)
+    private SteamVR_TrackedObject trackedObject;
+
+    void Awake()
     {
-        Debug.Log("Collided with " + collider.name);
-        var controller = collider.gameObject.GetComponent<StoryBlockController>();
-        controller.SetSelected(true);
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
     }
 
-    void OnTriggerExit(Collider collider)
+    void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collided with " + collider.name);
-        var controller = collider.gameObject.GetComponent<StoryBlockController>();
-        controller.SetSelected(false);
+        GetController(other.gameObject).SetSelected(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {        
+        GetController(other.gameObject).SetSelected(false);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int) trackedObject.index);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            other.attachedRigidbody.isKinematic = true;
+            other.gameObject.transform.SetParent(gameObject.transform);
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            other.attachedRigidbody.isKinematic = false;
+            other.gameObject.transform.SetParent(null);
+        }
+    }
+
+    private StoryBlockController GetController(GameObject gameObject)
+    {
+        return gameObject.GetComponent<StoryBlockController>();
     }
 }
