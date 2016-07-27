f34b3c24481fcb90d107d01032bed058d56786d2
rkettelerij <richard@mindloops.nl>
2016-07-27 10:30:29 +0200

    Extracted stories to separate model class

--- Assets/Scripts/StoryStacker.cs
+++ Assets/Scripts/StoryStacker.cs
@@ -6,9 +6,11 @@ public class StoryStacker : MonoBehaviour {
 	public GameObject storyBlockObject;
 
 	void Start () {
-		for (var y = 0; y < 10; y++) {
-			GameObject storyBlock = (GameObject) Instantiate(storyBlockObject, new Vector3(0, 0.50f + (y * 0.16f), 2.0f), Quaternion.identity);
-			storyBlock.GetComponent<StoryController> ().SetTextValue ("Issue " + (y + 1));
+		StoriesModel storiesModel = new StoriesModel ();
+
+		for (var i=0; i<storiesModel.Stories.Count; i++) {
+			GameObject storyBlock = (GameObject) Instantiate(storyBlockObject, new Vector3(0, 0.50f + (i * 0.16f), 2.0f), Quaternion.identity);
+			storyBlock.GetComponent<StoryController> ().SetTextValue (storiesModel.Stories[i].Title);
 		}
 	}
 }
