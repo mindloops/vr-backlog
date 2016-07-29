9bac24d39ceb273d838bfe7e220b5ac8ff716f93
tim prijn <tim prijn>
2016-07-29 22:35:24 +0200

    added 3d audio for deletion and appearance of new items (Mario Bros Style)

--- Assets/Scripts/BinTrigger.cs
+++ Assets/Scripts/BinTrigger.cs
@@ -4,9 +4,17 @@ using System.Collections;
 public class BinTrigger : MonoBehaviour {
 
 	public StoryLogic storyLogic;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     
 	void OnTriggerStay(Collider other)
 	{
         storyLogic.Remove(other.gameObject);
+        audioSource.Play();
     }
 }
